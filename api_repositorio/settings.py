5ac5a9065e03aa1eb0d0fcfa6eab8b95810d795b
zuri <matiaszurita931@gmail.com>
2023-12-11 14:03:54 -0300

    agregue otra url

--- api_repositorio/settings.py
+++ api_repositorio/settings.py
@@ -152,7 +152,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://front-repositorio.vercel.app"
+    "https://front-repositorio.vercel.app",
+    'https://front-repositorio-9b08x3h0c-zuris-projects.vercel.app'
 ]
 
 # In settings.py
